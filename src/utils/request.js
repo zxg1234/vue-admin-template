695c328b61d616c8dec74543d87304a95e40172a
3195383493@qq.com <zxg093017.>
2026-04-14 23:35:25 +0800

    修改axios封装的baseURL为动态写法

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,41 +1,39 @@
-import axios from 'axios';
-import store from '@/store';
+import axios from 'axios'
+import store from '@/store'
 
 const request = axios.create({
-  baseURL: '/api',
+  baseURL: process.env.VUE_APP_BASE_API,
   timeout: 10000
 })
 
 request.interceptors.request.use(
- (config)=>
-    {
-      config.headers['Authorization'] = `Bearer ${store.getters.token}`;
-      // Do something before the request is sent
-    return config;
-    }
+  (config) => {
+    config.headers['Authorization'] = `Bearer ${store.getters.token}`
+    // Do something before the request is sent
+    return config
+  }
   ,
-  (error)=>{// Do something with the request error
-     return Promise.reject(error);}
-);
+  (error) => { // Do something with the request error
+    return Promise.reject(error)
+  }
+)
 
 request.interceptors.response.use(
-  function (response) {
-    const {data,message,success} = response.data
+  function(response) {
+    const { data, message, success } = response.data
     console.log(success)
     console.log(message)
     console.log(data)
-    if(success === false){
+    if (success === false) {
       return Promise.reject(new Error(message || 'Error'))
-    }else{
-      
+    } else {
       return data
     }
   },
-  function (error) {
+  function(error) {
     // Any status codes that fall outside the range of 2xx cause this function to trigger
     // Do something with response error
-    return Promise.reject(error);
-  },
-);
-
-export default request;
+    return Promise.reject(error)
+  }
+)
+export default request
