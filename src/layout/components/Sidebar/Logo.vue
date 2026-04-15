3cb64d6db9d8d02ff3b159d8ec64aa0b6646d30c
3195383493@qq.com <zxg093017.>
2026-04-15 22:47:58 +0800

    修改logo

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -1,13 +1,8 @@
 <template>
   <div class="sidebar-logo-container" :class="{'collapse':collapse}">
     <transition name="sidebarLogoFade">
-      <router-link v-if="collapse" key="collapse" class="sidebar-logo-link" to="/">
-        <img v-if="logo" :src="logo" class="sidebar-logo">
-        <h1 v-else class="sidebar-title">{{ title }} </h1>
-      </router-link>
-      <router-link v-else key="expand" class="sidebar-logo-link" to="/">
-        <img v-if="logo" :src="logo" class="sidebar-logo">
-        <h1 class="sidebar-title">{{ title }} </h1>
+      <router-link key="collapse" class="sidebar-logo-link" to="/">
+        <img src="@/assets/common/logo.png" class="sidebar-logo">
       </router-link>
     </transition>
   </div>
@@ -20,12 +15,6 @@ export default {
     collapse: {
       type: Boolean,
       required: true
-    }
-  },
-  data() {
-    return {
-      title: 'Vue Admin Template',
-      logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
     }
   }
 }
@@ -55,8 +44,7 @@ export default {
     width: 100%;
 
     & .sidebar-logo {
-      width: 32px;
-      height: 32px;
+      width: 140px;
       vertical-align: middle;
       margin-right: 12px;
     }
@@ -75,6 +63,8 @@ export default {
 
   &.collapse {
     .sidebar-logo {
+      height: 32px;
+      width: 32px;
       margin-right: 0px;
     }
   }
